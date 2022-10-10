bbec44763e4af11420eef78f2da64fc65fc11e66
vyaasBaskar <nasascience9@gmail.com>
2022-10-10 11:40:34 -0700

    Save

--- get_weather.py
+++ get_weather.py
@@ -1,4 +1,10 @@
 import meteostat
+from meteostat import Point, Hourly
+from datetime import datetime, timedelta
+import matplotlib.pyplot as plt
 
 def get_weather(location, time):
-    return (humidity, windspeed, winddirection, temperature)
+    data = Hourly(location, time-timedelta(1/24, 0), time)
+    data = data.fetch()
+    return (data["temp"], data["rhum"], data["wdir"], data["wspd"], data["pres"], data["prcp"])
+print(get_weather(Point(49.2497, -123.1193, 70), datetime.now()))
